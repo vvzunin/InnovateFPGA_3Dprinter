54d0c4c212c5a8ece2808f9bc3cff24d5a4b28f1
Spieler <volodya12309@gmail.com>
2019-09-29 15:53:15 +0300

    remove some bugs

---
+++ FPGA/Quartus_project/endstop_filter.v
@@ -0,0 +1,28 @@
+module endstop_filter(	input 	wire	clk,
+								input		wire	in,
+								output	reg	out = 0);
+
+	reg [31:0] 	k 		= 0;
+	reg 			prev 	= 0;
+	
+	always @(posedge clk)
+	begin
+		if (in != out)
+		begin
+			if (k < 10000)
+			begin
+				if (in == prev)
+					k = k + 1;
+			end
+			else
+			begin
+				if (k == 10000)
+					out = prev;
+			end
+		end
+		else
+			k = 0;		
+		prev = in;
+	end
+								
+endmodule
